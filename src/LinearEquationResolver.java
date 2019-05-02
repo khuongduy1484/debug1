2383a8fe647c57d4b52cf2aa2d3324e2daa4f6de
khuongduy1484 <khuongduy1484@gmail.com>
2019-05-02 18:20:00 +0700

    commit

--- src/LinearEquationResolver.java
+++ src/LinearEquationResolver.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class LinearEquationResolver {
     public static void main(String[] args) {
         System.out.println("Linear Equation Resolver");
@@ -15,7 +14,7 @@ public class LinearEquationResolver {
 
         if (a != 0) {
             double solution = -b / a;
-            System.out.printf("The solution is: %f!", solution);
+            System.out.println("The solution is: "+ solution);
         } else {
             if (b == 0) {
                 System.out.print("The solution is all x!");
